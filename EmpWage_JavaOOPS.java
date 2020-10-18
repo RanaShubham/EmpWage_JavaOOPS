5425f701bb8b06e0b0c2049c0b8d77937dde3183
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 10:27:12 +0530

    Refactored the code to calculate monthly wage of the employee

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -1,28 +1,42 @@
 package EmpWage_JavaOOPS;
 
-public class EmpWage_JavaOOPS {
-	
+public class EmpWage_JavaOOPS 
+{	
 	//Constants
 	static final int WAGE_PER_HOUR = 20;
-	static final int FULL_DAY_HRS=8;
-	static final int PART_TIME_HRS=4;
+	static final int FULL_DAY_HRS = 8;
+	static final int PART_TIME_HRS = 4;
+	
+	//Variables
+	static int monthlyWage = 0;
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to employee wage computation");
-		int empAttendance = (int)Math.floor(Math.random()*10)%2;
 		
-		switch (empAttendance) 
+		for (int i=0; i<20; i++)
 		{
-			case  1:
-				System.out.println("Daily wage for today as full time emp is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
-				break;
-			case  2:
-				System.out.println("Daily wage for today as part time emp is: "+WAGE_PER_HOUR*PART_TIME_HRS);
-				break;
-			default :
-				System.out.println("Daily wage for today is 0");
+			int empAttendance = (int)Math.floor(Math.random()*10)%3;
+			
+			switch (empAttendance) 
+			{
+				case  1:
+					calcWage(WAGE_PER_HOUR*FULL_DAY_HRS);
+					break;
+				case  2:
+					calcWage(WAGE_PER_HOUR*PART_TIME_HRS);
+					break;
+				default :
+					calcWage(0);
+			}
 		}
+		System.out.println("Employee wage for the whole month is: "+monthlyWage);
 	}
-
+	
+	//To calculate monthly wage by adding daily salary
+	private static void calcWage (int wageToday)
+	{
+		monthlyWage = monthlyWage + wageToday;
+	}
 }
+

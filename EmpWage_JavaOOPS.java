accfef52570e13bedd8458018d0ecd44c7d9b575
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 10:38:26 +0530

    Refactor: calculating wages until a conditon of 100 working hours or 20 working days is met

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -6,15 +6,19 @@ public class EmpWage_JavaOOPS
 	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_DAY_HRS = 8;
 	static final int PART_TIME_HRS = 4;
+	static final int MAX_WORKING_DAYS = 20;
+	static final int MAX_WORKING_HRS = 100;
 	
 	//Variables
 	static int monthlyWage = 0;
+	static int workingHrs = 0;
+	static int workingDays = 0;
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to employee wage computation");
 		
-		for (int i=0; i<20; i++)
+		while (true)
 		{
 			int empAttendance = (int)Math.floor(Math.random()*10)%3;
 			
@@ -22,12 +26,22 @@ public class EmpWage_JavaOOPS
 			{
 				case  1:
 					calcWage(WAGE_PER_HOUR*FULL_DAY_HRS);
+					workingHrs = workingHrs + FULL_DAY_HRS;
+					workingDays++;
 					break;
 				case  2:
 					calcWage(WAGE_PER_HOUR*PART_TIME_HRS);
+					workingHrs = workingHrs + PART_TIME_HRS;
+					workingDays++;
 					break;
 				default :
+					workingDays++;
 					calcWage(0);
+			}
+			
+			if (workingHrs >= MAX_WORKING_HRS || workingDays >= MAX_WORKING_DAYS)
+			{
+				break;
 			}
 		}
 		System.out.println("Employee wage for the whole month is: "+monthlyWage);

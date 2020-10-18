ec8594f01849861d2c461e85a5455f88fda1c46c
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 10:07:03 +0530

    Added functionality to calulate the salary of employee when he works part time.

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -5,6 +5,7 @@ public class EmpWage_JavaOOPS {
 	//Constants
 	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_DAY_HRS=8;
+	static final int PART_TIME_HRS=4;
 
 	public static void main(String[] args) 
 	{
@@ -12,9 +13,11 @@ public class EmpWage_JavaOOPS {
 		int empAttendance = (int)Math.floor(Math.random()*10)%2;
 		
 		if (empAttendance == 1)
-		{
-			System.out.println("Daily wage for today is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
-		}
+			System.out.println("Daily wage for today as full time emp is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
+		else if (empAttendance == 2)
+			System.out.println("Daily wage for today as part time emp is: "+WAGE_PER_HOUR*PART_TIME_HRS);
+		else
+			System.out.println("Daily wage for today is 0");
 	}
 
 }

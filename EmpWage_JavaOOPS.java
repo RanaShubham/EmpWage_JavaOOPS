26ed5f32573d1a7b8880f7340f7bacfb893d7a09
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 09:58:26 +0530

    Adding the ability to check if employee is present or absent

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -5,6 +5,8 @@ public class EmpWage_JavaOOPS {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to employee wage computation");
+		int empAttendance = (int)Math.floor(Math.random()*10)%2;
+		System.out.println(empAttendance);
 	}
 
 }

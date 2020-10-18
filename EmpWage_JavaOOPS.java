94cb9e880b89d2fe2d5e74d19300019a618e7734
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 13:58:41 +0530

    Refactor: Using instance variables for calculating total salary

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -3,31 +3,31 @@ package EmpWage_JavaOOPS;
 interface IBM 
 {
 	//Constants
-	static final int WAGE_PER_HOUR = 30;
-	static final int FULL_DAY_HRS = 8;
-	static final int PART_TIME_HRS = 4;
-	static final int MAX_WORKING_DAYS = 20;
-	static final int MAX_WORKING_HRS = 100;
+	final int WAGE_PER_HOUR = 30;
+	final int FULL_DAY_HRS = 8;
+	final int PART_TIME_HRS = 4;
+	final int MAX_WORKING_DAYS = 20;
+	final int MAX_WORKING_HRS = 100;
 }
 
 interface Microsoft
 {
 	//Constants
-	static final int WAGE_PER_HOUR = 50;
-	static final int FULL_DAY_HRS = 9;
-	static final int PART_TIME_HRS = 3;
-	static final int MAX_WORKING_DAYS = 24;
-	static final int MAX_WORKING_HRS = 200;
+	final int WAGE_PER_HOUR = 50;
+	final int FULL_DAY_HRS = 9;
+	final int PART_TIME_HRS = 3;
+	final int MAX_WORKING_DAYS = 24;
+	final int MAX_WORKING_HRS = 200;
 }
 
 interface Flipkart
 {
 	//Constants
-	static final int WAGE_PER_HOUR = 15;
-	static final int FULL_DAY_HRS = 12;
-	static final int PART_TIME_HRS = 6;
-	static final int MAX_WORKING_DAYS = 30;
-	static final int MAX_WORKING_HRS = 300;
+	final int WAGE_PER_HOUR = 15;
+	final int FULL_DAY_HRS = 12;
+	final int PART_TIME_HRS = 6;
+	final int MAX_WORKING_DAYS = 30;
+	final int MAX_WORKING_HRS = 300;
 }
 
 
@@ -36,28 +36,31 @@ public class EmpWage_JavaOOPS implements IBM, Microsoft, Flipkart
 	//constants
 	static final int IS_FULL_TIME = 1;
 	static final int IS_PART_TIME = 2;
-	static final int IS_ABSENT = 0;
 	
 	//Variables
-	int monthlyWage = 0;
-	int workingHrs = 0;
-	int workingDays = 0;
-	int dailyWage = 0;
+	int monthlyWage;
+	int workingHrs;
+	int workingDays;
+	int dailyWage;
+	int wagePerHour;
+	int fullDayHours;
+	int partTimeHours;
+	int maxWorkingDays;
+	int maxWorkingHrs;
 
-	public static void main(String[] args) 
+	//Constructor
+	EmpWage_JavaOOPS(int wagePerHour, int fullDayHours, int partTimeHours, int maxWorkingDays, int maxWorkingHrs)
 	{
-		System.out.println("Welcome to employee wage computation");
-		System.out.println("Monthly wage for IBM employee is: "+ calcWage(IBM.FULL_DAY_HRS,IBM.MAX_WORKING_DAYS,IBM.MAX_WORKING_HRS,IBM.PART_TIME_HRS,IBM.WAGE_PER_HOUR));
-		System.out.println("Monthly wage for Microsoft employee is: "+ calcWage(Microsoft.FULL_DAY_HRS,Microsoft.MAX_WORKING_DAYS,Microsoft.MAX_WORKING_HRS,Microsoft.PART_TIME_HRS,Microsoft.WAGE_PER_HOUR));
-		System.out.println("Monthly wage for Flipkart employee is: "+ calcWage(Flipkart.FULL_DAY_HRS,Flipkart.MAX_WORKING_DAYS,Flipkart.MAX_WORKING_HRS,Flipkart.PART_TIME_HRS,Flipkart.WAGE_PER_HOUR));
-
+		this.wagePerHour = wagePerHour;
+		this.fullDayHours = fullDayHours;
+		this.partTimeHours = partTimeHours;
+		this.maxWorkingDays = maxWorkingDays;
+		this.maxWorkingHrs = maxWorkingHrs;
 	}
-	
+
 	//To calculate monthly wage
-	private static int calcWage (int fullTimeHrs, int maxDays, int maxHrs, int partTimeHrs, int wagePerHr)
-	{
-		EmpWage_JavaOOPS obj = new EmpWage_JavaOOPS();
-		
+	int calcTotalWage ()
+	{		
 		while (true)
 		{
 			int empAttendanceType = (int)Math.floor(Math.random()*10)%3;
@@ -65,28 +68,41 @@ public class EmpWage_JavaOOPS implements IBM, Microsoft, Flipkart
 			switch (empAttendanceType) 
 			{
 				case IS_FULL_TIME:
-					obj.dailyWage = wagePerHr * fullTimeHrs;
-					obj.monthlyWage = obj.monthlyWage + obj.dailyWage;
-					obj.workingHrs = obj.workingHrs + fullTimeHrs;
-					obj.workingDays++;
+					this.dailyWage = this.wagePerHour * this.fullDayHours;
+					this.monthlyWage = this.monthlyWage + this.dailyWage;
+					this.workingHrs = this.workingHrs + this.fullDayHours;
+					this.workingDays++;
 					break;
 				case IS_PART_TIME:
-					obj.dailyWage = wagePerHr * partTimeHrs;
-					obj.monthlyWage = obj.monthlyWage + obj.dailyWage;
-					obj.workingHrs = obj.workingHrs + partTimeHrs;
-					obj.workingDays++;
+					this.dailyWage = this.wagePerHour * this.partTimeHours;
+					this.monthlyWage = this.monthlyWage + this.dailyWage;
+					this.workingHrs = this.workingHrs + this.partTimeHours;
+					this.workingDays++;
 					break;
-				case IS_ABSENT:
-					obj.workingDays++;
+				default:
+					this.workingDays++;
 			}
 			
-			if (obj.workingHrs >= maxHrs || obj.workingDays >= maxDays)
+			if (this.workingHrs >= this.maxWorkingHrs || this.workingDays >= this.maxWorkingDays)
 			{
 				break;
 			}
 		}
 		
-		return obj.monthlyWage;
+		return this.monthlyWage;
+	}
+
+	//Main method
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to employee wage computation");
+		
+		EmpWage_JavaOOPS ibm = new EmpWage_JavaOOPS(IBM.WAGE_PER_HOUR, IBM.FULL_DAY_HRS, IBM.PART_TIME_HRS, IBM.MAX_WORKING_DAYS, IBM.MAX_WORKING_HRS);
+		EmpWage_JavaOOPS microsoft = new EmpWage_JavaOOPS(Microsoft.WAGE_PER_HOUR, Microsoft.FULL_DAY_HRS, Microsoft.PART_TIME_HRS, Microsoft.MAX_WORKING_DAYS, Microsoft.MAX_WORKING_HRS);
+		EmpWage_JavaOOPS flipkart = new EmpWage_JavaOOPS(Flipkart.WAGE_PER_HOUR, Flipkart.FULL_DAY_HRS, Flipkart.PART_TIME_HRS, Flipkart.MAX_WORKING_DAYS, Flipkart.MAX_WORKING_HRS);
+	
+		int [] totalWages = {ibm.calcTotalWage(),microsoft.calcTotalWage(),flipkart.calcTotalWage()};
+		
+		System.out.println(totalWages[0]+" "+totalWages[1]+" "+totalWages[2]);
 	}
 }
-

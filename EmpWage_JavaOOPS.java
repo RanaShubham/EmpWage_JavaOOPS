fd62a8160c37219da266b1756c1dff82446977c7
Shubham Rana <ranashubham7296@gmail.com>
2020-10-18 10:02:26 +0530

    Refactored 1_EmpCheck to calculate the daily wage of an employee if he is present

--- EmpWage_JavaOOPS.java
+++ EmpWage_JavaOOPS.java
@@ -1,12 +1,20 @@
 package EmpWage_JavaOOPS;
 
 public class EmpWage_JavaOOPS {
+	
+	//Constants
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_DAY_HRS=8;
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to employee wage computation");
 		int empAttendance = (int)Math.floor(Math.random()*10)%2;
-		System.out.println(empAttendance);
+		
+		if (empAttendance == 1)
+		{
+			System.out.println("Daily wage for today is: "+WAGE_PER_HOUR*FULL_DAY_HRS);
+		}
 	}
 
 }
